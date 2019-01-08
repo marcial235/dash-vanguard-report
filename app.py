86079d00f7d915f49741c22065ab9c0b1f9e79bd
marcial235 <marcial2005@gmail.com>
2019-01-08 11:27:30 -0600

    Cambios

--- app.py
+++ app.py
@@ -1217,7 +1217,7 @@ def display_page(pathname):
 external_css = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
                 "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
                 "//fonts.googleapis.com/css?family=Raleway:400,300,600",
-                "https://codepen.io/bcd/pen/KQrXdb.css",
+                "https://github.com/marcial235/dash-vanguard-report/blob/master/modelo.css",
                 "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
 
 for css in external_css:
